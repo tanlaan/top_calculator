9d7c9ba4061774b2534488275a0f00afdd2be200
Chris Bolas <tanlaan33@gmail.com>
2020-10-17 16:08:26 -0700

    Broken functionality, but getting closer!

--- calc.js
+++ calc.js
@@ -38,6 +38,63 @@ function operate(operator,a, b) {
 
 }
 
+let operator = ""
+let firstValue = null
+let secondValue = null
+
 function updateScreenValue(value){
-    document.getElementById('screen-value').innerHTML = value
+    switch(value){
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            if(operator == ""){
+                // Working on first value
+                if(firstValue == null){
+                    firstValue = value
+                } else{
+                    firstValue += value
+                }
+            } else {
+                // Working on second value
+                if(secondValue == null){
+                    secondValue = value
+                } else {
+                    secondValue += value
+                }
+            }
+            break;
+        case '+':
+            operator = '+'
+            break;
+        case '-':
+            operator = '-'
+            break;
+        case '/':
+            operator = '/'
+            break;
+        case '*':
+            operator = '*'
+            break;
+        case '=':
+            document.getElementById('screen-value').innerHTML = operate(operator, Number(firstValue), Number(secondValue))
+            operator = ""
+            firstValue = null
+            secondValue = null
+            break;
+    }
+    if(value == '='){
+
+    } else if(secondValue == null){
+        document.getElementById('screen-value').innerHTML = firstValue
+    } else {
+        document.getElementById('screen-value').innerHTML = secondValue
+    }
 }
+
