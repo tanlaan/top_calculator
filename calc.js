1588c13846d86254457e512b5df2d314569cfee8
Chris Bolas <tanlaan33@gmail.com>
2020-10-16 19:29:20 -0700

    Made framework. Added html & css file.

---
+++ calc.js
@@ -0,0 +1,38 @@
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    if( b == 0 ){
+        return NaN
+    } else {
+        return a / b
+    }
+}
+
+function operate(operator,a, b) {
+    switch(operator) {
+        case '+':
+            return add(a, b)
+            break;
+        case '-':
+            return subtract(a, b)
+            break;
+        case '*':
+            return multiply(a, b)
+            break;
+        case '/':
+            return divide(a, b)
+            break;
+        default:
+    }
+
+}
